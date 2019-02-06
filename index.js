e581b5193894b9e598f09a17d70681c976432f1a
Andrés Alonzo y Alonzo <5524124+aalonzolu@users.noreply.github.com>
2019-02-05 18:17:51 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var app = angular.module('climaApp', [ 'n3-pie-chart']);
 app.controller('climaCtrl', function($scope, $http) {
-  $scope.firstName= "John";
-  $scope.lastName= "Doe";
+ 
   $scope.options = {thickness: 5, mode: "gauge", total: 100};
 
-  $scope.ahoratemp = [{label: "Temp", value: 0, color: "#5e90e0", suffix: "â„ƒ"}];
+  $scope.ahoratemp = [{label: "Temp", value: 0, color: "#5e90e0", suffix: "℃"}];
   $scope.ahorahumedad = [{label: "Humedad", value: 0, color: "#5e90e0", suffix: "%"}];
   
   $scope.getahora = ()=>{
